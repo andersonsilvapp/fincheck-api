655e49b1b65ef48860dc70ef55c2f21ddf40feae
Anderson <andersonsilvapp@gmail.com>
2023-06-29 17:32:31 -0300

    feat: list, update and remove bankAccount

--- src/modules/bank-accounts/bank-accounts.module.ts
+++ src/modules/bank-accounts/bank-accounts.module.ts
@@ -1,9 +1,11 @@
 import { Module } from '@nestjs/common';
-import { BankAccountsService } from './bank-accounts.service';
+import { BankAccountsService } from './services/bank-accounts.service';
 import { BankAccountsController } from './bank-accounts.controller';
+import { ValidateBankAccountOwnewshipService } from './services/validateBankAccountOwnership.service';
 
 @Module({
   controllers: [BankAccountsController],
-  providers: [BankAccountsService]
+  providers: [BankAccountsService, ValidateBankAccountOwnewshipService],
+  exports: [ValidateBankAccountOwnewshipService],
 })
 export class BankAccountsModule {}
